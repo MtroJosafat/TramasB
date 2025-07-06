af5b744c3cac55538385c93041e8108ce43438d2
MtroJosafat <ricardojosafat@gmail.com>
2025-07-06 08:37:41 -0600

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function showSection(sectionId) {
-  document.querySelectorAll('.section').forEach(section => {
-    section.classList.remove('active');
-  });
-  document.getElementById(sectionId).classList.add('active');
-}
-
-// Mostrar la sección "Inicio" al cargar la página
-window.onload = () => showSection('home');
